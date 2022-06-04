48ae9415ad6eb9aa8cc67cd75074f3e5af1531e6
Jonah Saysourivong <jsaysourivong@gmailcom>
2022-06-04 00:08:55 -0700

    Add project files.

---
+++ BookWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Create a category table with the name categories
+        public DbSet<Category> Categories { get; set; }
+    }
+}
