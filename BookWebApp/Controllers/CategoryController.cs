712ff7d6150dbc13274cea1f7ec3b69ad3e8040a
Jonah Saysourivong <jsaysourivong@gmailcom>
2022-08-15 15:58:09 -0700

    Add Toastr Alerrts to Category page for create, edit, and deleting categories.

--- BookWebApp/Controllers/CategoryController.cs
+++ BookWebApp/Controllers/CategoryController.cs
@@ -36,9 +36,88 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created sucessfully";   
             return RedirectToAction("Index");
         }
         return View(obj);
+    }
+
+    // GET
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        // The three lines of code following this commenta are three ways you can retreive a category from the database using the primary key
+        var categoryFromDb = _db.Categories.Find(id);
+        //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+        //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+        if(categoryFromDb == null)
+        {
+            return NotFound(); 
+        }            
+
+        return View(categoryFromDb);
+    }
+
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name ", "The DisplayOrder cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated sucessfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+    // GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        // The three lines of code following this commenta are three ways you can retreive a category from the database using the primary key
+        var categoryFromDb = _db.Categories.Find(id);
+        //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+        //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+
+    // POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if(obj == null)
+        {
+            return NotFound();
+        }
+
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted sucessfuly";
+        return RedirectToAction("Index");
     }
 }
 
